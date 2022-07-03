d0531d72d66550847d988c6b4a7b9b53580c8cb8
Byeong-Su <72130113+Byeong-Su@users.noreply.github.com>
2022-07-03 20:44:21 +0900

    Add File [time-todo]

--- time-todo/src/screens/CalendarView.js
+++ time-todo/src/screens/CalendarView.js
@@ -12,6 +12,19 @@ const styles = StyleSheet.create({
 const CalendarView = () => {
   return (
     <Calendar
+      //사용자의 시간 데이터를 받아서
+      //시간에 따라 마킹 진하기 다르게
+      markingType={'period'}
+      markedDates={{
+        '2022-07-15': {marked: true, dotColor: '#50cebb'},
+        '2022-07-16': {marked: true, dotColor: '#50cebb'},
+        '2022-07-21': {startingDay: true, color: '#50cebb', textColor: 'white'},
+        '2022-07-22': {color: '#70d7c7', textColor: 'white'},
+        '2022-07-23': {color: '#70d7c7', textColor: 'white', marked: true, dotColor: 'white'},
+        '2022-07-24': {color: '#70d7c7', textColor: 'white'},
+        '2022-07-25': {endingDay: true, color: '#50cebb', textColor: 'white'}
+      }}
+
       style={styles.calendar}
       theme={{
         //selectedDayBackgroundColor: 'red',
@@ -25,3 +38,6 @@ const CalendarView = () => {
 };
 
 export default CalendarView;
+
+//캘린더 관련 문서
+//https://github.com/wix/react-native-calendars
